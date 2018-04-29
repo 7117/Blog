cb50c52dc74ec28257780d919574bebef51c51ff
7117 <18701558590@139.com>
2018-04-29 11:59:34 +0800

    rbac

--- backend/views/adminuser/index.php
+++ backend/views/adminuser/index.php
@@ -35,28 +35,28 @@ $this->params['breadcrumbs'][] = $this->title;
             // 'password_reset_token',
 
             ['class' => 'yii\grid\ActionColumn',
-            		'template'=>'{view} {update} {resetpwd} {privilege}',
-            		'buttons'=>[
-            				'resetpwd'=>function($url,$model,$key)
-            				{
-            					$options=[
-            							'title'=>Yii::t('yii','重置密码'),
-            							'aria-label'=>Yii::t('yii','重置密码'),
-            							'data-pjax'=>'0',
-            							];
-            					return Html::a('<span class="glyphicon glyphicon-lock"></span>',$url,$options);
-            				},
-            				
-            				'privilege'=>function($url,$model,$key)
-            				{
-            					$options=[
-            							'title'=>Yii::t('yii','权限'),
-            							'aria-label'=>Yii::t('yii','权限'),
-            							'data-pjax'=>'0',
-            					];
-            					return Html::a('<span class="glyphicon glyphicon-user"></span>',$url,$options);
-            				},
-            				
+                    'template'=>'{view} {update} {resetpwd} {privilege}',
+                    'buttons'=>[
+                            'resetpwd'=>function($url,$model,$key)
+                            {
+                                $options=[
+                                        'title'=>Yii::t('yii','重置密码'),
+                                        'aria-label'=>Yii::t('yii','重置密码'),
+                                        'data-pjax'=>'0',
+                                        ];
+                                return Html::a('<span class="glyphicon glyphicon-lock"></span>',$url,$options);
+                            },
+                            
+                            'privilege'=>function($url,$model,$key)
+                            {
+                                $options=[
+                                        'title'=>Yii::t('yii','权限'),
+                                        'aria-label'=>Yii::t('yii','权限'),
+                                        'data-pjax'=>'0',
+                                ];
+                                return Html::a('<span class="glyphicon glyphicon-user"></span>',$url,$options);
+                            },
+                            
     ],
     ],
         ],
